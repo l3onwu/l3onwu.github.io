51bd0261ec6263f92409856608cc907096da0b8d
Leon Wu <85681107+l3onwu@users.noreply.github.com>
2023-03-03 15:39:15 +1100

    Feat: Revamp styles for more whitespace

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function scrollToPageTwo() {
+  document
+    .getElementById("page-two")
+    .scrollIntoView({ behavior: "smooth", inline: "nearest" });
+}
+
+function scrollToPageOne() {
+  window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+}
